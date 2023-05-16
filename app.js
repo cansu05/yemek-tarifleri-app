ec9b7abd536591e46e4d14de4fe5264e6af96b65
cansu <cansuque5@gmail.com>
2023-05-16 20:53:50 +0300

    verilerin dinamik eklenmesi

--- app.js
+++ app.js
@@ -1,18 +1,87 @@
 const buttonRecipe = document.querySelector("#button-recipe");
-const cardRecipe = document.querySelector("#card-recipe");
+const cardRecipe = document.querySelector(".card-recipe");
 
+let recipesList = [];
 
-const recipes = [
-    {
-        foodName: "Brokoli Çorbası",
-        img: "https://images.pexels.com/photos/5644943/pexels-photo-5644943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        serving: "4",
-        time: "30",
-        ingredients: "",
-        making: "",
-    }
-]
+buttonRecipe.addEventListener(
+  "click",
+  (toggleRecipe = () => {
+    cardRecipe.classList.toggle("show");
+  })
+);
 
-buttonRecipe.addEventListener("click", getRecipe = () => {
-    cardRecipe.classList.toggle("hide")
-})
+const getRecipes = () => {
+  fetch("./recipes.json")
+    .then((res) => res.json())
+    .then((recipes) => (recipesList = recipes));
+};
+
+getRecipes();
+
+const createRecipesItemHtml = () => {
+  const recipesListElement = document.querySelector(".recipes__list");
+  let recipesListHtml = "";
+  recipesList.forEach((recipe) => {
+    recipesListHtml += `
+    
+    <div class="card-item recipes__list">
+      <img class="image" src="${recipe.img}" alt="" />
+      <h2>${recipe.foodName}</h2>
+          
+      <div class="button">
+        <button id="button-recipe" onclick="toggleRecipe">
+          <i class="button-icon fa-solid fa-angles-down"></i>
+        </button>
+      </div>
+    </div>
+    <!-- card recipe başlangıç-->
+    <div class="card-recipe">
+      <div class="recipe-icon">
+        <div class="recipe-serving-icon">
+          <i class="fa-solid fa-utensils"></i>
+          <h5>${recipe.serving} Kişilik</h5>
+        </div>
+        <div class="recipe-time-icon">
+          <i class="fa-regular fa-clock"></i>
+          <h5>
+            Pişme Süresi <br />
+            ${recipe.time}dk
+          </h5>
+        </div>
+      </div>
+      <div class="recipe">
+        <h3 id="recipe-title">${recipe.foodName}  Tarifi İçin Malzemeler</h3>
+        <ul id="ingredients">
+          <li>Lorem, ipsum dolor.</li>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Lorem, ipsum dolor.</li>
+        </ul>
+        <h3 id="recipe-title">${recipe.foodName} Tarifi Nasıl Yapılır?</h3>
+        <ol id="making">
+          <li>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </li>
+          <li>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </li>
+          <li>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </li>
+          <li>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </li>
+        </ol>
+      </div>
+    </div>
+    <!-- card recipe bitiş-->
+
+    `;
+  });
+
+  recipesListElement.innerHTML = recipesListHtml;
+};
+
+setTimeout(() => {
+  createRecipesItemHtml();
+}, 100);
